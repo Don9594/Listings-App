cd3db85b277f13fbfc58d1b9ba34fb55191b061c
Don Wijesinghe <menuwan.wijesinghe@gmail.com>
2022-03-22 18:21:20 -0500

    Initial Commit

---
+++ RecipeListApp/RecipeListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListAppApp.swift
+//  RecipeListApp
+//
+//  Created by Don wijesinghe on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
