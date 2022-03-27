06524c81521cc789b8578a7ea4f8ba971704d3f9
Don Wijesinghe <menuwan.wijesinghe@gmail.com>
2022-03-27 08:59:38 -0500

    Added RecipeTabView as entry point to the app.

---
+++ RecipeListApp/Views/RecipeTabView.swift
@@ -0,0 +1,37 @@
+//
+//  RecipeTabView.swift
+//  RecipeListApp
+//
+//  Created by Don wijesinghe on 3/27/22.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        
+        TabView{
+            
+            Text("Featured Items").tabItem{
+                VStack{
+                    Image(systemName: "star.fill")
+                    Text("Featured")
+                }
+            }
+            
+            RecipeListView().tabItem{
+                VStack{
+                    Image(systemName: "list.bullet.rectangle")
+                    Text("List")
+                }
+            }
+            
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
